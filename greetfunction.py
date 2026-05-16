c6c638ea47e4f43902eaf3ace2cdc84ee1f57cc6
18Divyanshh <hirkanedivyansh@gmail.com>
2026-05-16 12:16:24 +0530

    greet function

---
+++ greetfunction.py
@@ -0,0 +1,9 @@
+user_name = input(" what is your name ")
+
+def greet(name):
+    print(" hello " , name )
+
+
+
+greet(user_name)
+
---
+++ sum.function.py
@@ -0,0 +1,7 @@
+def sum():
+    x = int(input(" what is x ? "))
+    y = int(input(" what is y ? "))
+    print( x + y)
+
+
+sum()
