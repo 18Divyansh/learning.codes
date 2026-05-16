1a5002a8c2af2e415313503d518dff050c68cf48
18Divyanshh <hirkanedivyansh@gmail.com>
2026-05-16 13:37:04 +0530

    largest number

---
+++ largestnumber.py
@@ -0,0 +1,16 @@
+def largest_number(a,b,c):
+    if a > b and a > c:
+        return a 
+    elif b > a and b > c:
+        return b 
+    else:
+        return c 
+
+a = int(input(" what is a ?"))
+b = int(input(" what is b "))
+c = int(input(" what is c "))
+
+
+
+result = largest_number(a,b,c) 
+print(" the largest number is " , result)
