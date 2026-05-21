7c9691361d09bcf0170953589a4d67f07196f7dd
18Divyanshh <hirkanedivyansh@gmail.com>
2026-05-21 22:56:29 +0530

    noting

---
+++ prime.py
@@ -0,0 +1,11 @@
+n = input(" what is your number ")
+
+def prime():
+    if 6*n+1 == x and x is == 0:
+        print(" the number is prime ")
+    else:
+        print("number is not prime ") 
+
+
+
+prime()           
