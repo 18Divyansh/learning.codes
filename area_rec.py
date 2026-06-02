629dcde711240a23e997bbf86da3a7fddeca34d9
18Divyanshh <hirkanedivyansh@gmail.com>
2026-06-02 23:05:42 +0530

    function for area of rectangle

---
+++ area_rec.py
@@ -0,0 +1,11 @@
+def area (w,l):
+
+    return w*l
+
+
+x = int(input("what is width"))
+y = int(input("what is lenght "))
+
+result = area(x,y)
+
+print(f"The area of a rectangle with width {x} and length {y} is: {result}")
